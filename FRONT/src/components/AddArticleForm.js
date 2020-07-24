fc0137f345099febf115625101803d3a80636302
Laura Domingues <lauradomingues@MBP-de-Laura.lan>
2020-07-24 21:48:45 +0200

    Création d'un composans pour le formulaire d'ajout

---
+++ FRONT/src/components/AddArticleForm.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+class AddArticleForm extends Component {
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addArticle = (event) => {
+    event.preventDefault();
+    console.log("titre : " + this.state.titre);
+
+    const url = "http://localhost:3000/articles";
+    const postsData = {
+      titre: this.state.titre,
+      contenu: this.state.contenu,
+      Categorie: this.state.Categorie,
+    };
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(postsData),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .catch((error) => console.error("Error:", error))
+      .then((response) => console.log("Success:", response));
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.addArticle}>
+        <input
+          type="text"
+          name="titre"
+          onChange={this.handleChange}
+          placeholder="titre"
+        />
+        <br />
+        <textarea
+          type="text"
+          name="contenu"
+          onChange={this.handleChange}
+          placeholder="Contenu"
+        />
+        <br />
+        <input
+          type="text"
+          name="Categorie"
+          onChange={this.handleChange}
+          placeholder="Categorie"
+        />
+        <button type="submit">Add</button>
+      </form>
+    );
+  }
+}
+
+export default AddArticleForm;
--- FRONT/src/components/App.js
+++ FRONT/src/components/App.js
@@ -1,7 +1,7 @@
 //import logo from './logo.svg';
 //import './App.css';
 import List from './list'
-import Article from "./Article";
+import AddArticleForm from "./AddArticleForm";
 
 import React, { Component } from "react";
 
@@ -39,28 +39,6 @@ class App extends Component {
       );
   }
 
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("titre : " + this.state.titre);
-
-    const url = "http://localhost:3000/articles";
-    const postsData = { titre: this.state.titre, contenu: this.state.contenu, Categorie: this.state.Categorie};
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify(postsData), 
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((res) => res.json())
-      .catch((error) => console.error("Error:", error))
-      .then((response) => console.log("Success:", response));
-  };
-
-
-
   render() {
     const { error, isLoaded, postsData } = this.state;
     
@@ -72,20 +50,15 @@ class App extends Component {
     } else {
   
       return (
-        <div>
+        <div className='App'>
           {console.log(postsData)}
 
           <List posts={postsData} />
 
           <hr />
-
           <h2>Ajouter un article</h2>
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" name="titre" onChange={this.handleChange} placeholder="titre" /><br />            
-            <textarea type="text" name="contenu" onChange={this.handleChange} placeholder="Contenu" /><br />
-            <input type="text" name="Categorie" onChange={this.handleChange} placeholder="Categorie" />
-            <button type="submit">Add</button>
-          </form>
+          <AddArticleForm />
+          
         </div>
       );
     }
