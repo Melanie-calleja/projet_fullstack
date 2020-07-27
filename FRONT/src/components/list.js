4f10f65c2214473533c931a52978862166726d49
w.mariejoseph <w.marie-joseph@totemnumerique.com>
2020-07-27 14:08:03 +0200

    Ajout de l'idTag

--- FRONT/src/components/list.js
+++ FRONT/src/components/list.js
@@ -8,7 +8,7 @@ class List extends Component {
         <ul>
           {
             this.props.posts.map((post, index) => 
-            <Article id={post._id} titre={post.titre} contenu={post.contenu} categorie={post.idCategorie} tags={post.tag} date={post.Date}  state={post.state}  key={index} />
+            <Article id={post._id} titre={post.titre} contenu={post.contenu} categorie={post.idCategorie} tag={post.idTag} date={post.Date}  state={post.state}  key={index} />
             )
           }
         </ul>
