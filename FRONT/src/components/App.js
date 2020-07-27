d5a7803cf30be055d6c5aabb976335689ea49fb0
w.mariejoseph <w.marie-joseph@totemnumerique.com>
2020-07-27 14:49:08 +0200

    Permettre l'ajout d'un tag

--- FRONT/src/components/App.js
+++ FRONT/src/components/App.js
@@ -1,8 +1,10 @@
 //import logo from './logo.svg';
 //import './App.css';
-import List from './list'
+import List from "./list";
 import AddArticleForm from "./AddArticleForm";
-import Categories from './Categories';
+import SearchByCategories from "./SearchByCategories";
+import Category from "./Category";
+import Tag from "./Tag";
 
 import React, { Component } from "react";
 
@@ -12,8 +14,14 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      postsData: null
+      postsData: null,
+      article: null,
+      idArticle: "",
+      idCategory: "",
+      idTag: "",
     };
+    this.selectArticleChange = this.selectArticleChange.bind(this);
+    this.searchByTitre = this.searchByTitre.bind(this);
   }
 
   componentDidMount() {
@@ -23,45 +31,112 @@ class App extends Component {
         Accept: "application/json",
       },
     })
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            postsData: result,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
+        .then((res) => res.json())
+        .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                postsData: result,
+              });
+            },
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error,
+              });
+            }
+        );
   }
 
+  selectArticleChange(event) {
+    this.setState({ idArticle: event.target.value });
+  }
+
+  cancelSearch(event) {
+    this.setState({ article: null });
+  }
+
+  searchByTitre(event) {
+    event.preventDefault();
+    console.log(this.state.idArticle);
+    const url = "http://localhost:3000/articles/" + this.state.idArticle;
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+        .then((res) => res.json())
+        .then(
+            (result) => {
+              this.setState({
+                article: result,
+              });
+            },
+            (error) => {
+              console.error("Error:", error);
+            }
+        );
+  }
+
+
+
   render() {
-    const { error, isLoaded, postsData } = this.state;
-    
+    const { error, isLoaded, postsData, article } = this.state;
+    var articlesList = null;
+
+    if (article != null) {
+      articlesList = (
+          <div>
+            {article.titre}
+            <h3>{article.titre}</h3>
+            <p>{article.contenu}</p>
+            <p>Categorie :{<Category idCategory={article.idCategorie} />}</p>
+            <p>Tag : {<Tag idTag={article.idTag} />}</p>
+            <p>Date : {article.date}</p>
+            <hr />
+            <button className="cancelSearch" onClick={(e) => this.cancelSearch()}>
+              retour
+            </button>
+          </div>
+      );
+    } else {
+      articlesList = <List posts={postsData} />;
+    }
 
     if (error) {
       return <div>Erreur : {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Chargement…</div>;
     } else {
-  
       return (
-        <div className='App'>
-          
+          <div className="App">
+            {articlesList}
 
-          <List posts={postsData} />
+            <hr />
+            <div>
+              <h2>Ajouter un article</h2>
+              <AddArticleForm />
+              <hr />
+              <h2>Recherche par Categories</h2>
+              <form onSubmit={this.searchByCategories}>
 
-          <hr />
-          <h2>Ajouter un article</h2>
-          <AddArticleForm />
-          {/* <h2>Categories</h2>
-          <Categories/> */}
-        </div>
+                <SearchByCategories />
+
+                <button type="submit">Rechercher</button>
+              </form>
+              <hr />
+              <h2>Recherche par titre</h2>
+              <form onSubmit={this.searchByTitre}>
+                <select onChange={this.selectArticleChange}>
+                  {postsData.map((element) => {
+                    return <option value={element._id}>{element.titre}</option>;
+                  })}
+                </select>
+                <button type="submit">Rechercher</button>
+              </form>
+            </div>
+          </div>
       );
     }
   }
